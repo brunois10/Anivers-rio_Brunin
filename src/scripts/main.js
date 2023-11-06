04634bc9156cdd20a960bd1393f918883ef1d410
Bruno Guimarães <brunogdepaula@hotmail.com>
2023-11-06 15:34:51 -0300

    Projeto finalizado

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const dataDoEvento = new Date("May 23, 2024 21:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+    const mesesAteOEvento = Math.floor(diasAteOEvento / 30);
+    const diasRestantes = diasAteOEvento % 30.5;
+
+    document.getElementById('contador').innerHTML = `${mesesAteOEvento}M ${diasRestantes}D ${horasAteOEvento}H ${minutosAteOEvento}M ${segundosAteOEvento}S`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
